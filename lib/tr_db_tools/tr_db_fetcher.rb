2c9f9d1252e1b2a5175fccd7da99b823adfdb41a
Julien Faucher <julien.faucher@gmail.com>
2017-09-03 13:32:10 -0400

    Add method to fetch while handling the data type (useful for queries that return arrays). Can probably be merged with fetch, but some testing required

--- lib/tr_db_tools/tr_db_fetcher.rb
+++ lib/tr_db_tools/tr_db_fetcher.rb
@@ -37,6 +37,39 @@ module TrDbFetcher
       end
     end
   end
+
+  #Similar to fetch, but returns complex types like array as valid ruby arrays
+  def self.fetch_with_type(args)
+    #Merge of self.fetch and of this stackoverflow answer : https://stackoverflow.com/a/30948357
+    #This might be mergeable with fetch, but it will need testing before confirmation
+    connection = ActiveRecord::Base.connection
+    @type_map ||= PG::BasicTypeMapForResults.new(connection.raw_connection)
+    if args.respond_to? :upcase # duck type for string
+        result = connection.execute(args)
+        result.type_map = @type_map
+        return result.to_a
+    else
+      to_a          = args.fetch(:to_a, true)
+      connection    = args.fetch(:connection, ActiveRecord::Base.connection)
+      query         = args.fetch(:query)
+      single_result = args.fetch(:single_result, false)
+      if single_result # return single result if passed as argument. Otherwise, return an array
+        result = connection.execute(query).first.values.first
+        result.type_map = @type_map
+        return result
+      else
+        if to_a
+          result = connection.execute(query)
+          result.type_map = @type_map
+          return result.to_a
+        else
+          result = connection.execute(query)
+          result.type_map = @type_map
+          return result
+        end
+      end
+    end
+  end
   
   def self.fetch_symbolize_keys(args)
     result = TrDbFetcher.fetch(args)
